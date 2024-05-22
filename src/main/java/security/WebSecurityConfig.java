8c739ca8d3c791c4b48e64206d59bbe446b2d18f
Dymayu <dymayura@gmail.com>
2024-05-22 23:13:21 +0200

    added input type="hidden" for _csrf token

--- src/main/java/security/WebSecurityConfig.java
+++ src/main/java/security/WebSecurityConfig.java
@@ -1,11 +1,27 @@
 package security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()));
+
+
+        return http.build();
+    }
 
 
 }
